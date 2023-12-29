012deaba21084fbe588afee5d8530178b6ec5ebe
Abaljerind <abaljerinda@gmail.com>
2023-12-29 17:34:37 +0700

    add style for question & answer

--- src/Components/QuestionAnswer.jsx
+++ src/Components/QuestionAnswer.jsx
@@ -1,8 +1,10 @@
 export default function QuestionAnswer(props) {
   return (
     <article className="mt-6 relative">
-      <p className="question">{props.question}</p>
-      <p className="answer">{props.answer}</p>
+      <p className="question text-darkPurple font-bold text-base">
+        {props.question}
+      </p>
+      <p className="answer text-grayishPurple text-sm">{props.answer}</p>
       <hr class="h-px my-8 bg-lightPink border-0"></hr>
       <img
         className="absolute -top-1 cursor-pointer right-0"
