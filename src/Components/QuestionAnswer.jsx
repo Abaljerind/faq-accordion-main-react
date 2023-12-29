2cfcfc5b1d2a251136ee713b6272f2e2012778cc
Abaljerind <abaljerinda@gmail.com>
2023-12-29 17:03:27 +0700

    wrong commit

--- src/Components/QuestionAnswer.jsx
+++ src/Components/QuestionAnswer.jsx
@@ -1,7 +1,7 @@
 export default function QuestionAnswer(props) {
   return (
-    <article className="mt-6">
-      <p className="question mt-4">{props.question}</p>
+    <article className="">
+      <p className="question">{props.question}</p>
       <p className="answer">{props.answer}</p>
     </article>
   );
