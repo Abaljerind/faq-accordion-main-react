9c1e72f809b570506f6de81e2803b63d01e5e5e6
Abaljerind <abaljerinda@gmail.com>
2023-12-29 16:58:10 +0700

    add QuestionAnswer component + send data to props

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,3 +1,5 @@
+import QuestionAnswer from "./QuestionAnswer";
+
 export default function Header() {
   return (
     <main className="h-[720px] flex justify-center items-center">
@@ -10,6 +12,22 @@ export default function Header() {
         <h1 className="font-bold text-5xl text-darkPurple absolute top-5 left-20">
           FAQs
         </h1>
+        <QuestionAnswer
+          question="What is Frontend Mentor, and how will it help me?"
+          answer="Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building."
+        />
+        <QuestionAnswer
+          question="Is Frontend Mentor free?"
+          answer="Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels."
+        />
+        <QuestionAnswer
+          question="Can I use Frontend Mentor projects in my portfolio?"
+          answer="Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!"
+        />
+        <QuestionAnswer
+          question="How can I get help if I'm stuck on a Frontend Mentor challenge?"
+          answer="The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members."
+        />
       </section>
     </main>
   );
