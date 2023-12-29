a058ebbea5dd527d7194477fbccb5dba8ecaae38
Abaljerind <abaljerinda@gmail.com>
2023-12-29 17:35:54 +0700

    create Images component & add the codes

---
+++ src/Components/Images.jsx
@@ -0,0 +1,11 @@
+export default function Images() {
+  return (
+    <>
+      <img
+        className="absolute -top-1 cursor-pointer right-0"
+        src={require("../images/icon-plus.svg").default}
+        alt="plus"
+      />
+    </>
+  );
+}
