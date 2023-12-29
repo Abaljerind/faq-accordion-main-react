94d261d2fad4e594b9a4543ed9aab2dd5a79aa16
Abaljerind <abaljerinda@gmail.com>
2023-12-29 17:29:28 +0700

    add hr & add style: article, hr & img

--- src/Components/QuestionAnswer.jsx
+++ src/Components/QuestionAnswer.jsx
@@ -1,8 +1,14 @@
 export default function QuestionAnswer(props) {
   return (
-    <article className="">
+    <article className="mt-6 relative">
       <p className="question">{props.question}</p>
       <p className="answer">{props.answer}</p>
+      <hr class="h-px my-8 bg-lightPink border-0"></hr>
+      <img
+        className="absolute -top-1 cursor-pointer right-0"
+        src={require("../images/icon-plus.svg").default}
+        alt="plus"
+      />
     </article>
   );
 }
