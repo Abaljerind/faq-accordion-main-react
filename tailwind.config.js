68252fff91b75e4840eefe70c4825561f9298cf7
Abaljerind <abaljerinda@gmail.com>
2023-12-29 14:19:21 +0700

    add 3 new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightPink: "hsl(275, 100%, 97%)",
+        grayishPurple: "hsl(292, 16%, 49%)",
+        darkPurple: "hsl(292, 42%, 14%)",
+      },
+    },
   },
   plugins: [],
 };
