c54802dbc3c2136de1f8fecf90b587dcfe4a0b8a
Abaljerind <abaljerinda@gmail.com>
2023-12-29 16:52:28 +0700

    add html elements with props

--- src/Components/QuestionAnswer.jsx
+++ src/Components/QuestionAnswer.jsx
@@ -1,1 +1,8 @@
-export default function QuestionAnswer() {}
+export default function QuestionAnswer(props) {
+  return (
+    <article>
+      <p className="question">{props.question}</p>
+      <p className="answer">{props.answer}</p>
+    </article>
+  );
+}
