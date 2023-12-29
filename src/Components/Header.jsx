30ea8cc6691c23e20a8439d42227a344b22b200f
Abaljerind <abaljerinda@gmail.com>
2023-12-29 20:43:10 +0700

    add divider component & correction on the last question

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,3 +1,4 @@
+import Divider from "./Divider";
 import QuestionAnswer from "./QuestionAnswer";
 
 export default function Header() {
@@ -17,16 +18,19 @@ export default function Header() {
           question="What is Frontend Mentor, and how will it help me?"
           answer="Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building."
         />
+        <Divider />
         <QuestionAnswer
           question="Is Frontend Mentor free?"
           answer="Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels."
         />
+        <Divider />
         <QuestionAnswer
           question="Can I use Frontend Mentor projects in my portfolio?"
           answer="Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!"
         />
+        <Divider />
         <QuestionAnswer
-          question="How can I get help if I'm stuck on a Frontend Mentor challenge?"
+          question="How can I get help if I'm stuck on a challenge?"
           answer="The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members."
         />
       </section>
